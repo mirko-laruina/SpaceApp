4e4630f74fdf3a8dcd99576cfca5f903365d903e
Mirko <mirkoletto@gmail.com>
2019-10-19 13:16:41 +0200

    Added sidebar

--- aquagang/src/index.js
+++ aquagang/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+import SideBar from "./sidebar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div id="App">
+        <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <div id="page-wrap">
+        CONTENT
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
