923c9405307631e43a0e1d8bbe676e62dda79694
Mirko <mirkoletto@gmail.com>
2019-10-19 14:43:45 +0200

    Map working

--- aquagang/src/index.js
+++ aquagang/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import SideBar from "./sidebar";
+import MapContainer from './MapContainer.js';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
 
 function App() {
   return (
     <div id="App">
-        <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+        <SideBar className="sidebar" pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
       <div id="page-wrap">
-        CONTENT
+        <MapContainer />
       </div>
     </div>
   );
@@ -18,3 +21,4 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
--- aquagang/src/sidebar.js
+++ aquagang/src/sidebar.js
@@ -4,7 +4,7 @@ import { slide as Menu } from "react-burger-menu";
 export default props => {
   return (
     // Pass on our props
-    <Menu right="true" {...props}>
+    <Menu right={true} {...props}>
       <a className="menu-item" href="/">
         Home
       </a>
