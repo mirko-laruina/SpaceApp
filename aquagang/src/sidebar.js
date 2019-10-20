cf163e30149e96240a1c3dbec218a4d4073f4399
ericaraffa <ericaraffa@virgilio.it>
2019-10-20 04:21:16 +0000

    aggiunta widgets

--- aquagang/src/sidebar.js
+++ aquagang/src/sidebar.js
@@ -1,25 +1,83 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
+import Slider from '@material-ui/core/Slider';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+
 
 export default props => {
+
+  
+
   return (
+
     // Pass on our props
     <Menu right={true} {...props}>
       <a className="menu-item" href="/">
         Home
       </a>
 
-      <a className="menu-item" href="/burgers">
-        Burgers
+      <a className="menu-item" href="/popolazione">
+        <FormControlLabel
+          value="end"
+          control={<Checkbox color="primary" />}
+          label="Population"
+          labelPlacement="end"
+        />
       </a>
 
-      <a className="menu-item" href="/pizzas">
-        Pizzas
+      <a className="menu-item" href="/aree verdi">
+      <FormControlLabel
+          value="end"
+          control={<Checkbox color="primary" />}
+          label="Green areas"
+          labelPlacement="end"
+        />
       </a>
 
-      <a className="menu-item" href="/desserts">
-        Desserts
+      <a className="menu-item" href="/curve di livello">
+      <FormControlLabel
+          value="end"
+          control={<Checkbox color="primary" />}
+          label="Levels"
+          labelPlacement="end"
+        />
       </a>
+
+      <a className="menu-item" href="/time">
+        Time
+        <br/>
+        <Slider
+          defaultValue={30}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={2019}
+          max={2119}
+        />
+      </a>
+
+      <a className="menu-item" href="/water">
+        Water
+        <br/>
+        <Slider
+          defaultValue={0}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={0}
+          max={100}
+        />
+      </a>
+
+
+      
     </Menu>
   );
 };
